e8f455dbe7db5e4fab800d5a20c6cf310405cec3
haiswork <whaibin01@126.com>
2018-04-14 07:54:52 +0800

    add connect&listen net.Conn

--- net.go
+++ net.go
@@ -1,19 +1,45 @@
 package hnet
 
-import "net"
+import (
+	"net"
+	"time"
+)
 
-func ListenSocket(network, addr string, callback func(socket *Socket)) error {
+func Listen(network, addr string, callback func(conn net.Conn)) error {
 	listener, err := net.Listen(network, addr)
 	if err != nil {
 		return err
 	}
+	var tempDelay time.Duration
 	for {
-		c, e := listener.Accept()
-		if e != nil {
-			return e
+		conn, err := listener.Accept()
+		if err != nil {
+			if ne, ok := err.(net.Error); ok && ne.Temporary() {
+				if tempDelay == 0 {
+					tempDelay = 5 * time.Millisecond
+				} else {
+					tempDelay *= 2
+				}
+				if max := time.Second; tempDelay > max {
+					tempDelay = max
+				}
+				time.Sleep(tempDelay)
+				continue
+			}
+			return err
 		}
-		callback(NewSocket(c))
+		callback(conn)
 	}
+}
+
+func Connect(network, addr string) (net.Conn, error) {
+	return net.Dial(network, addr)
+}
+
+func ListenSocket(network, addr string, callback func(socket *Socket)) error {
+	return Listen(network, addr, func(conn net.Conn) {
+		callback(NewSocket(conn))
+	})
 }
 
 func ConnectSocket(network, addr string) (*Socket, error) {
