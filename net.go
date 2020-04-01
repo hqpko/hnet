e6d3c8cba725d1a8b1f2022baf02c5d142c2f8c3
hqpko <whaibin01@hotmail.com>
2020-04-01 14:09:47 +0800

    net: default tcp conn

--- net.go
+++ net.go
@@ -4,32 +4,22 @@ import (
 	"net"
 )
 
-func Listen(network, addr string, callback func(conn net.Conn)) error {
-	listener, err := net.Listen(network, addr)
-	if err != nil {
+func ListenSocket(addr string, handler func(socket *Socket)) error {
+	if listener, err := net.Listen("tcp", addr); err != nil {
 		return err
-	}
-	for {
-		if conn, err := listener.Accept(); err != nil {
-			return err
-		} else {
-			callback(conn)
+	} else {
+		for {
+			if conn, err := listener.Accept(); err != nil {
+				return err
+			} else {
+				handler(NewSocket(conn))
+			}
 		}
 	}
 }
 
-func Connect(network, addr string) (net.Conn, error) {
-	return net.Dial(network, addr)
-}
-
-func ListenSocket(network, addr string, callback func(socket *Socket)) error {
-	return Listen(network, addr, func(conn net.Conn) {
-		callback(NewSocket(conn))
-	})
-}
-
-func ConnectSocket(network, addr string) (*Socket, error) {
-	c, e := net.Dial(network, addr)
+func ConnectSocket(addr string) (*Socket, error) {
+	c, e := net.Dial("tcp", addr)
 	if e != nil {
 		return nil, e
 	}
